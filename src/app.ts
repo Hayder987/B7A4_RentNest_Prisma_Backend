ba45c944f8c609d7d1bba477a5974108515710ec
Hayder Ali <hayderbd4290@gmail.com>
2026-07-06 01:41:11 +0600

    create env config and root route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,24 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
+import httpStatus from "http-status";
 
 const app:Application = express();
 
+
+// using middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// root route
+app.get("/", (re: Request, res: Response) => {
+  res.status(httpStatus.OK).json({
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Welcome to Rent Nest",
+    data: [],
+  });
+});
 
 
 
