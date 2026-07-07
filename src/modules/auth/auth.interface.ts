73672d9b9d3f28cb9d5b324bbfd4e3168145b197
Hayder Ali <hayderbd4290@gmail.com>
2026-07-07 14:07:20 +0600

    Fix: user role admin created issue with zod validation

--- src/modules/auth/auth.interface.ts
+++ src/modules/auth/auth.interface.ts
@@ -1,11 +1,16 @@
-import { Role } from "../../../generated/prisma/enums";
+import { Role, UserStatus } from "../../../generated/prisma/enums";
 
 export interface IRegisterUser {
   name: string;
   email: string;
   password: string;
-  role?: Role;
+  role?: Extract<Role, "LANDLORD" | "TENANT">;
 }
+
+export type IUpdateUser = {
+  role?: Role;
+  status?: UserStatus;
+};
 
 export interface ILoginUser {
     email: string,
--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -1,13 +1,14 @@
 
 import { z } from "zod";
-import { Role } from "../../../generated/prisma/enums";
+import { Role, UserStatus } from "../../../generated/prisma/enums";
 
+// create user zod schema req.body
 export const createUserValidationSchema = z.object({
   name: z
     .string()
     .trim()
     .min(3, "Name must be at least 3 characters")
-    .max(40, "Name cannot exceed 50 characters"),
+    .max(40, "Name cannot exceed 40 characters"),
 
   email: z
     .email("Please provide a valid email address")
@@ -19,12 +20,25 @@ export const createUserValidationSchema = z.object({
     .min(6, "Password must be at least 6 characters")
     .max(100, "Password cannot exceed 100 characters"),
 
-  role: z.enum(Role).optional(),
-});
+  role: z.enum([Role.LANDLORD, Role.TENANT]).optional(),
+}).strict();
 
-export const updateUserValidationSchema =
-  createUserValidationSchema.partial();
 
+// Update user zod schema req.body
+export const updateUserValidationSchema = z
+  .object({
+    role: z.enum(Role).optional(),
+    status: z.enum(UserStatus).optional(),
+  })
+  .strict()
+  .refine(
+    (data) => data.role !== undefined || data.status !== undefined,
+    {
+      message: "At least one field (role or status) is required.",
+    }
+  );
+
+  // login user zod schema req.body
 export const loginValidationSchema = z.object({
   email: z
     .email("Please provide a valid email address")
